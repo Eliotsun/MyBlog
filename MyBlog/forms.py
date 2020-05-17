9b70bd119785407aa85337116899af38b6d50377
Eliotsun <sunshibin@163.com>
2020-05-17 14:22:11 +0800

    Update forms.py

--- MyBlog/forms.py
+++ MyBlog/forms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# __auther__ = 'sunshibin'
+# __auther__ = 'eliot'
 
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, ValidationError
